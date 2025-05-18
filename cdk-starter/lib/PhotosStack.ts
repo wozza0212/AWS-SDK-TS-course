dc04d352bd440651de58d2c9fd88c6985c027f46
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2025-05-18 20:01:46 +0100

    rename bucket

--- cdk-starter/lib/PhotosStack.ts
+++ cdk-starter/lib/PhotosStack.ts
@@ -6,8 +6,12 @@ export class PhotosStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'PhotosBucket', {
-        bucketName: 'photobucket-bvjhsbxjkabxjkas'
-    })
+    const PhotosBucket = new Bucket(this, 'PhotosBucket3', {
+        bucketName: 'photobucket-bvjhsbxjkabxjkas3'
+    });
+    // If PhotosBucket is changed, AWS will delete and replace the cloud formation stack
+    
+    // To rename bucket logial if manually, create a const for bucket 
+    (PhotosBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucketRenamable')
   }
 }
