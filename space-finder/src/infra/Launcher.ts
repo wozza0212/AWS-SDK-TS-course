06b6a7ed40562454cc1e270fb05eaf628ee95935
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2026-02-16 19:42:48 +0000

    deploying lambda stack

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -6,6 +6,8 @@ import { ApiStack } from "./stacks/ApiStack";
 
 const app = new App();
 
-new DataStack(app, 'DataStack')
-const lambdaStack = new LambdaStack(app, 'LambdaStack')
+const dataStack = new DataStack(app, 'DataStack')
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+    spacesTable: dataStack.spacesTable
+})
 new ApiStack(app, 'ApiStack', {helloLambdaIntegration: lambdaStack.helloLambdaIntegration})
