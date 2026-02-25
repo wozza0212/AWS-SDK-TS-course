2938a28226bb6d64460884e01d32fbf6055dab97
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2026-02-25 21:19:06 +0000

    ad ScanCommand

---
+++ space-finder/src/services/spaces/GetSpaces.ts
@@ -0,0 +1,17 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+const getSpaces = async (
+  event: APIGatewayProxyEvent,
+  ddbClient: DynamoDBClient,
+): Promise<APIGatewayProxyResult> => {
+  const result = await ddbClient.send(
+    new ScanCommand({
+      TableName: process.env.TABLE_NAME,
+    }),
+  );
+  console.log(result.Items);
+  return { statusCode: 201, body: JSON.stringify(result.Items) };
+};
+
+export default getSpaces;
